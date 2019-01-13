93c25a0a8f0673b72b01eb6888504969640a31a1
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2019-01-13 18:05:24 -0500

    Added new page for detail alarm add and added button funcionality to add a new alarm

---
+++ app/src/main/java/com/restez/alexbochel/resteasy/AlarmDetailPageActivity.java
@@ -0,0 +1,13 @@
+package com.restez.alexbochel.resteasy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmDetailPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_detail_page);
+    }
+}
--- app/src/main/java/com/restez/alexbochel/resteasy/AlarmScreen.java
+++ app/src/main/java/com/restez/alexbochel/resteasy/AlarmScreen.java
@@ -1,51 +1,43 @@
 package com.restez.alexbochel.resteasy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
-
-import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class AlarmScreen extends AppCompatActivity {
 
-    private static final String TAG = "Main Activity";
+    private FloatingActionButton newAlarmButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        newAlarmButton = findViewById(R.id.floatingActionButton);
+
+        newAlarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                openNewAlarmDetailPageActivity();
             }
         });
-
-        Log.d(TAG, "On Create: Started");
 
         initRecyclerView();
     }
 
     private void initRecyclerView() {
-
-        Log.d(TAG, "Main Alarm View Creation Started");
-
         ArrayList<AlarmModel> dummyAlarmModelList = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             Date dummyDate = new Date();
 
             AlarmModel dummyAlarm = new AlarmModel(dummyDate, "Dummy", "Dummy",
@@ -56,6 +48,11 @@ public class AlarmScreen extends AppCompatActivity {
         RecyclerViewAdapter scrollableAlarmsViewAdapter = new RecyclerViewAdapter(dummyAlarmModelList);
         scrollableAlarmsView.setAdapter(scrollableAlarmsViewAdapter);
         scrollableAlarmsView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void openNewAlarmDetailPageActivity() {
+        Intent intent = new Intent(this, AlarmDetailPageActivity.class);
+        startActivity(intent);
     }
 
 }
