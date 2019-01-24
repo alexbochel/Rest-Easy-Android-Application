7d753cdb56b794de9e3bf07e1d42e1d4408a6b01
Alex Bochel <alexbochel@Alexs-MacBook-Air.local>
2019-01-24 17:28:04 -0500

    Fixed crashing problem and adjusted layout on alarm detail screen

--- app/src/main/java/com/restez/alexbochel/resteasy/AlarmDetailPageActivity.java
+++ app/src/main/java/com/restez/alexbochel/resteasy/AlarmDetailPageActivity.java
@@ -1,13 +1,32 @@
 package com.restez.alexbochel.resteasy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AlarmDetailPageActivity extends AppCompatActivity {
+
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_detail_page);
+
+        backButton = findViewById(R.id.back_button);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnToMainAlarmPage();
+            }
+        });
+    }
+
+    private void returnToMainAlarmPage() {
+        Intent intent = new Intent(this, AlarmScreen.class);
+        startActivity(intent);
     }
 }
